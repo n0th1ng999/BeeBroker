35eaf24a637bfc1a88a9b1d03d130ab29570d14e
Francisco Gameiro <gameirofrancisco6@gmail.com>
2024-10-24 12:21:35 +0100

    Change method

--- webapp/src/main.ts
+++ webapp/src/main.ts
@@ -1,12 +1,14 @@
+import './assets/main.css'
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-//import './style.css'
+
 import App from './App.vue'
 import router from './router'
 
-const pinia = createPinia()
+const app = createApp(App)
 
-createApp(App)
-.use(router)
-.use(pinia)
-.mount('#app')
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
