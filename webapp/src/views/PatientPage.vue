fba0a61b7a85bd5dbe3c1b76333b620b19aa4e5f
Francisco Gameiro <gameirofrancisco6@gmail.com>
2024-11-17 11:16:06 +0000

    Colocação de dados na página do paciente

--- webapp/src/views/PatientPage.vue
+++ webapp/src/views/PatientPage.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import PatientStatisics from "@/components/PatientStatisics.vue";
+import { usePatientStore } from "@/stores/patient";
 </script>
 
 <template>
@@ -36,28 +37,28 @@ import PatientStatisics from "@/components/PatientStatisics.vue";
 export default {
   data() {
     return {
-      //Nota: Temporário. Estes valores estarão dentro de uma store
-      heartRateHistory: [
-        60, 72, 84, 78, 65, 75, 80, 70, 76, 82, 68, 74, 86, 72,
-      ],
-      glycemicLevelsHistory: [
-        90, 105, 88, 110, 95, 100, 120, 98, 102, 85, 92, 115, 108, 99,
-      ],
-      arterialPressureHistory: [
-        36.5, 36.8, 37.2, 36.9, 36.6, 37.0, 37.4, 36.7, 37.1, 36.3, 36.5, 37.6,
-        37.0, 36.8,
-      ],
-      bodyTemperatureHistory: [
-        36.5, 36.8, 37.2, 36.9, 36.6, 37.0, 37.4, 36.7, 37.1, 36.3, 36.5, 37.6,
-        37.0, 36.8,
-      ],
-      respiratoryRateHistory: [
-        12, 14, 16, 15, 13, 14.5, 15.5, 13.5, 14, 16.5, 15, 14.2, 13.8, 14.8,
-      ],
-      oxygenSaturationHistory: [
-        98, 96, 94, 97, 99, 95, 96.5, 97.5, 98.5, 93, 95.5, 97, 96, 98,
-      ],
+      patientsStore: usePatientStore(),
     };
+  },
+  computed: {
+    heartRateHistory() {
+      return this.patientsStore.getPatient(+this.$route.params.id).heartRate 
+    },
+    glycemicLevelsHistory() {
+      return this.patientsStore.getPatient(+this.$route.params.id).glycemicLevels 
+    },
+    arterialPressureHistory() {
+      return this.patientsStore.getPatient(+this.$route.params.id).arterialPressure 
+    },
+    bodyTemperatureHistory() {
+      return this.patientsStore.getPatient(+this.$route.params.id).bodyTemperature 
+    },
+    respiratoryRateHistory() {
+      return this.patientsStore.getPatient(+this.$route.params.id).respiratoryRate 
+    },
+    oxygenSaturationHistory() {
+      return this.patientsStore.getPatient(+this.$route.params.id).oxygenSaturation 
+    },
   },
 };
 </script>
